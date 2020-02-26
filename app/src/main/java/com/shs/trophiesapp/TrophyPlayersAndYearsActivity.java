8d92c91db78ee1ea175738b2eda17921b6dde501
shstrophies <shstrophies@github.com>
2020-02-26 08:28:34 -0800

    add players and years page... first draft

--- app/src/main/java/com/shs/trophiesapp/TrophyPlayersAndYearsActivity.java
+++ app/src/main/java/com/shs/trophiesapp/TrophyPlayersAndYearsActivity.java
@@ -1,6 +1,5 @@
 package com.shs.trophiesapp;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -11,7 +10,7 @@ import androidx.cardview.widget.CardView;
 
 import com.shs.trophiesapp.utils.Utils;
 
-public class TrophyDetailsActivity extends AppCompatActivity {
+public class TrophyPlayersAndYearsActivity extends AppCompatActivity {
 
     private TextView tvTitle;
     private ImageView img;
@@ -20,23 +19,19 @@ public class TrophyDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cardview_trophy_details);
+        setContentView(R.layout.trophy_players_and_years_activity);
 
-        cardView = findViewById(R.id.cardview_trophy_details_id);
-        tvTitle = findViewById(R.id.txt_title);
-
-        img = findViewById(R.id.trophythumbnail);
+        tvTitle = findViewById(R.id.trophy_players_and_years_title);
+        img = findViewById(R.id.trophy_players_and_years_thumbnail);
 
         //Receive data
         Intent intent = getIntent();
         String sport_name = intent.getExtras().getString("sport_name");
-        int year = intent.getExtras().getInt("year");
         String tr_title = intent.getExtras().getString("tr_title");
         String tr_image_url = intent.getExtras().getString("tr_image_url");
         int color = intent.getExtras().getInt("color");
 
         tvTitle.setText(tr_title);
-
         Utils.imageFromUrl(img, tr_image_url);
         cardView.setBackgroundColor(color);
 
