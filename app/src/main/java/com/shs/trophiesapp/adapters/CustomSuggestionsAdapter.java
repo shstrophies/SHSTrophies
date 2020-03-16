8f32e02c51474a6267b884ccf01d513ca847a7af
shstrophies <shstrophies@github.com>
2020-03-15 18:41:55 -0700

    add custom suggestion adapter

---
+++ app/src/main/java/com/shs/trophiesapp/adapters/CustomSuggestionsAdapter.java
@@ -0,0 +1,97 @@
+package com.shs.trophiesapp.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.mancj.materialsearchbar.adapter.SuggestionsAdapter;
+import com.shs.trophiesapp.R;
+import com.shs.trophiesapp.database.entities.Trophy;
+import com.shs.trophiesapp.database.relations.SportWithTrophies;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mancj on 27.01.17.
+ */
+
+public class CustomSuggestionsAdapter extends SuggestionsAdapter<Trophy, CustomSuggestionsAdapter.SuggestionHolder> {
+
+    public CustomSuggestionsAdapter(LayoutInflater inflater) {
+        super(inflater);
+    }
+
+    @Override
+    public int getSingleViewHeight() {
+        return 80;
+    }
+
+    @Override
+    public SuggestionHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = getLayoutInflater().inflate(R.layout.item_custom_suggestion, parent, false);
+        return new SuggestionHolder(view);
+    }
+
+    @Override
+    public void onBindSuggestionHolder(Trophy suggestion, SuggestionHolder holder, int position) {
+        holder.title.setText(suggestion.getTitle());
+//        holder.subtitle.setText("The player is " + suggestion.trophies.() + "$");
+    }
+
+    /**
+     * <b>Override to customize functionality</b>
+     * <p>Returns a filter that can be used to constrain data with a filtering
+     * pattern.</p>
+     * <p>
+     * <p>This method is usually implemented by {@link androidx.recyclerview.widget.RecyclerView.Adapter}
+     * classes.</p>
+     *
+     * @return a filter used to constrain data
+     */
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                String term = constraint.toString();
+                if(term.isEmpty())
+                    suggestions = suggestions_clone;
+                else {
+                    suggestions = new ArrayList<>();
+                    for (Trophy item: suggestions_clone)
+                        if(item.getTitle().toLowerCase().contains(term.toLowerCase()))
+                            suggestions.add(item);
+                }
+                results.values = suggestions;
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                suggestions = (ArrayList<Trophy>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    static class SuggestionHolder extends RecyclerView.ViewHolder{
+        protected TextView title;
+        protected TextView subtitle;
+        protected ImageView image;
+
+        public SuggestionHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            subtitle = (TextView) itemView.findViewById(R.id.subtitle);
+        }
+    }
+
+}
+
